3c4f1d28a7d20ebbbd06e1946d353d216bd2acc7
marcokeilwagen <marco.keilwagen@nordakademie.de>
2025-12-15 18:32:22 +0000

    feat: add matMul package and Distribution class

---
+++ src/main/java/project/matMul/Distribution.java
@@ -0,0 +1,29 @@
+package project.matMul;
+
+import java.util.Arrays;
+
+public class Distribution {
+
+    private final int size;
+
+    public Distribution(Rnd rnd, int size){
+        this.size = size;
+    }
+
+    @Override
+    public void multiply() {
+
+    }
+
+    @Override
+    public void clearC(){
+        for(int i = 0; i < size; i++){
+            Arrays.fill(c[i], 0.0);
+        }
+    }
+
+    @Override
+    public double peek() {
+        return c[0][0];
+    }
+}
